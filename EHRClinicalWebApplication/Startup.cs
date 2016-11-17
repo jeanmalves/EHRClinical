096866ca3e0833993957ab7f1c140aa82ebdca00
Jeam Alves <jeanmalves@gmail.com>
2016-11-17 00:08:34 -0200

    Add project files.

---
+++ EHRClinicalWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EHRClinicalWebApplication.Startup))]
+namespace EHRClinicalWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
