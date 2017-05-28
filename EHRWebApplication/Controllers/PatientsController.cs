d0d23202ffa2d4dd1041cb28fa4165077e077ea6
Jeam Alves <jeanmalves@gmail.com>
2017-05-28 19:23:28 -0300

    Adicionado cadastro de paciente.

---
+++ EHRWebApplication/Controllers/PatientsController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Model.DAO;
+using EHRWebApplication.Models;
+using Model.BLL;
+
+namespace EHRWebApplication.Controllers
+{
+    public class PatientsController : Controller
+    {
+        private ClinicalEntities db = new ClinicalEntities();
+
+        // GET: Patients
+        public ActionResult Index()
+        {
+            var patients = PatientBLL.GetPatients();
+            return View(patients);
+        }
+
+        // GET: Patients/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var patient = PatientBLL.GetPatientById(id);
+
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(patient);
+        }
+
+        // GET: Patients/Create
+        public ActionResult Create()
+        {
+            //ViewBag.UserId = new SelectList(db.Users, "Id", "UserName");
+            return View(new PatientViewModel());
+        }
+
+        // POST: Patients/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,Birth,sex,UserId,EHR")] PatientViewModel pa)
+        {
+            if (ModelState.IsValid)
+            {
+                Patient patient = new Patient();
+                patient.FirstName = pa.FirstName;
+                patient.LastName = pa.LastName;
+                patient.sex = pa.Sex;
+                
+                bool update = PatientBLL.UpdatePatient(patient);
+
+                if (update)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+           // ViewBag.UserId = new SelectList(db.Users, "Id", "UserName", patient.UserId);
+            return View(pa);
+        }
+
+        // GET: Patients/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patient patient = db.Patients.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserId = new SelectList(db.Users, "Id", "UserName", patient.UserId);
+            return View(patient);
+        }
+
+        // POST: Patients/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,Birth,sex,UserId,EHR")] Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(patient).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserId = new SelectList(db.Users, "Id", "UserName", patient.UserId);
+            return View(patient);
+        }
+
+        // GET: Patients/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patient patient = db.Patients.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+
+        // POST: Patients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var delete = PatientBLL.DeletePatient(id);
+
+            if (delete)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ EHRWebApplication/Models/PatientViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Model.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EHRWebApplication.Models
+{
+    public class PatientViewModel
+    {
+        [Required(ErrorMessage = "Campo obrigatório.")]
+        [Display(Name = "Nome")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório.")]
+        [Display(Name = "Sobrenome")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório.")]
+        [Display(Name = "Data de nascimento")]
+        public DateTime? Birth { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório.")]
+        [Display(Name = "Sexo")]
+        public short Sex { get; set; }
+
+        public Dictionary<short, string> SexList
+        {
+            get { return SexDictionary.SexList; }
+            set { }
+        }
+    }
+}
