a23d526bea69b6e38b857d22804614f33d397079
Jeam Alves <jeanmalves@gmail.com>
2017-07-09 17:18:35 -0300

    Ajustes no banco.

--- EHRClinicalDB/Features.sql
+++ EHRClinicalDB/Features.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Features]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [name] VARCHAR(150) NOT NULL, 
+    [Name] VARCHAR(150) NOT NULL, 
     [Description] VARCHAR(MAX) NULL, 
     [DisplayMenu] SMALLINT NULL DEFAULT 0,
 	[TemplateId] INT NOT NULL, 
@@ -9,6 +9,7 @@
     [CreatedAt] DATETIME NOT NULL, 
     [UpdatedAt] DATETIME NULL, 
     [UserId] INT NOT NULL, 
+    [status] SMALLINT NOT NULL DEFAULT 1, 
     CONSTRAINT [FK_Features_ToOperationalsTemplates] FOREIGN KEY (TemplateId) REFERENCES OperationalsTemplates(Id), 
     CONSTRAINT [FK_Features_ToUsers] FOREIGN KEY (UserId) REFERENCES [Users](Id)
 )
