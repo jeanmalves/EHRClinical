df41e906eda3da11eac65fb0fa9ecaf1eaeb2f78
Jeam Alves <jeanmalves@gmail.com>
2017-07-23 10:43:23 -0300

    Adicionado tabela dinâmica para lista de dados de um atributo.

---
+++ EHRClinicalDB/DataListAttribute.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[DataListAttribute]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Key] INT NOT NULL, 
+    [Value] VARCHAR(50) NOT NULL,
+	[Term] VARCHAR(20) NULL, 
+    [AttributeId] INT NOT NULL,  
+    CONSTRAINT [FK_DataListAttribute_ToTemplateAttributes] FOREIGN KEY (AttributeId) REFERENCES [TemplateAttributes](Id)
+)
