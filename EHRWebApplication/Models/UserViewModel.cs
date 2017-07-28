02936ddeb65fbfa78ff52674f13faa72bcdadac0
Jeam Alves <jeanmalves@gmail.com>
2017-07-28 00:18:33 -0300

    Adicionado viewmodel para edição.

--- EHRWebApplication/Models/UserViewModel.cs
+++ EHRWebApplication/Models/UserViewModel.cs
@@ -22,4 +22,25 @@ namespace EHRWebApplication.Models
 
         public int Access { get; set; }
     }
+
+    public class UserEditViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório.")]
+        [Display(Name = "Usuário")]
+        public string UserName { get; set; }
+        
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Password { get; set; }
+
+        [Display(Name = "Organização")]
+        public string Organization { get; set; }
+
+        public int Access { get; set; }
+    }
 }
