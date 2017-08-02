a8184d39e8bf20cbd61972e3e0710ff133c7968e
Jeam Alves <jeanmalves@gmail.com>
2017-08-02 20:21:22 -0300

    ViewModel para lista e detalhes de consultas.

---
+++ EHRWebApplication/Models/PatientRecordViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Model.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EHRWebApplication.Models
+{
+    public class PatientRecordViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Data")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime? CreatedAt { get; set; }
+
+        [Display(Name = "Procedimento")]
+        public string OptName { get; set; }
+
+        [Display(Name = "Médico")]
+        public string Doctor { get; set; }
+
+    }
+
+    public class PatientRecordDetailsViewModel
+    {
+        public int Id { get; set; }
+
+        public int PatientId { get; set; }
+
+        [Display(Name = "Data")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime? CreatedAt { get; set; }
+
+        [Display(Name = "Procedimento")]
+        public string OptName { get; set; }
+
+        [Display(Name = "Médico")]
+        public string Doctor { get; set; }
+
+        public ICollection<Data> Data { get; set; }
+    }
+}
