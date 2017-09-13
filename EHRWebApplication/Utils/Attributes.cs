8794a46f22874428ec1c98cd0cdf2213cf8d237a
Jeam Alves <jeanmalves@gmail.com>
2017-09-12 23:54:37 -0300

    Configuração para autorização personalizada.

---
+++ EHRWebApplication/Utils/Attributes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EHRWebApplication.Utils
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, Inherited = true, AllowMultiple = true)]
+    public class AdministradorAttribute : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (base.AuthorizeCore(httpContext))
+            {
+                if (HttpContext.Current.User.Identity.Name != null)
+                {
+                    var user = Model.BLL.UserBLL.GetUserByEmail(HttpContext.Current.User.Identity.Name);
+
+                    if (user != null)
+                    {
+                        return user.RoleGroupID == (int)Model.DAO.Roles.ADMIN;
+                    }
+                }
+            }
+            
+            return false;
+        }
+    }
+}
