0abfc7e0f6f8f2f41c5059e464436769d7e5196f
Jeam Alves <jeanmalves@gmail.com>
2017-09-19 00:21:35 -0300

    Adicionado recurso funcionalidade.

---
+++ EHRWebApplication/Controllers/FeaturesController.cs
@@ -0,0 +1,112 @@
+﻿using EHRWebApplication.Models;
+using Model.BLL;
+using Model.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EHRWebApplication.Controllers
+{
+    [Utils.Administrador]
+    public class FeaturesController : MainController
+    {
+        // GET: Features
+        public ActionResult Index()
+        {
+            var features = FeatureBLL.GetFeatures()
+                .Select(l => new FeatureViewModel
+                {
+                    Id = l.Id,
+                    Feature = l.Name,
+                    Description = l.Description,
+                    CreatedAt = l.CreatedAt,
+                    TemplateName = l.OperationalsTemplate.Name,
+                    DisplayMenu = l.DisplayMenu,
+                    DisplayMenuLabel = l.DisplayMenu == 1 ? "Sim" : "Não",
+                    Status = l.status,
+                    StatusLabel = l.status == 1 ? "Ativo" : "Inativo",
+                    RoleGroup = l.FeatureGroups.FirstOrDefault(f => f.FeatureID == l.Id).RolesGroup.Id,
+                    RoleGroupLabel = RolesDictionary.getValue(
+                                                        (short)l.FeatureGroups
+                                                                .FirstOrDefault(f => f.FeatureID == l.Id)
+                                                                .RolesGroup.Id)
+                });
+
+            return View(features);
+        }
+
+        // GET: Features/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Features/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Features/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Features/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Features/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Features/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Features/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ EHRWebApplication/Models/FeatureViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EHRWebApplication.Models
+{
+    public class FeatureViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Funcionalidade")]
+        public string Feature { get; set; }
+
+        [Display(Name= "Descrição")]
+        public string Description { get; set; }
+
+        [Display(Name = "Criado em")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime? CreatedAt { get; set; }
+
+        [Display(Name = "EHR Template")]
+        public string TemplateName { get; set; }
+
+        public short? DisplayMenu { get; set; }
+
+        [Display(Name = "Exibir no menu")]
+        public string DisplayMenuLabel{ get; set; }
+
+        public short Status { get; set; }
+
+        [Display(Name = "Status")]
+        public string StatusLabel { get; set; }
+
+        public int RoleGroup { get; set; }
+
+        [Display(Name = "Grupo de Usuário")]
+        public string RoleGroupLabel { get; set; }
+    }
+}
